aaa883d4708864c8392eaa98d74c90d0108f41f8
Toluwalope Coast <rodneytoluwalope@gmail.com>
2021-08-10 15:37:16 +0100

    changed the template path for PUT, DELETE & GET

--- main/urls.py
+++ main/urls.py
@@ -5,15 +5,15 @@ from django.urls import path
 urlpatterns = [
 
     path('message/', create_message),
-    path('user/<int:pk>', message_detail),
+    path('message/<int:pk>', message_detail),
     path('group/', create_group),
     path('group/<int:pk>', group_detail),
     path('transaction/', create_transaction),
-    path('user/<int:pk>', transaction_detail),
+    path('transaction/<int:pk>', transaction_detail),
     path('sender/', create_sender),
     path('sender/<int:pk>', sender_detail),
     path('template/', create_template),
-    path('user/<int:pk>', template_detail),
+    path('template/<int:pk>', template_detail),
     path('contact/', create_contact),
-    path('user/<int:pk>', contact_detail),
+    path('contact/<int:pk>', contact_detail),
 ]
